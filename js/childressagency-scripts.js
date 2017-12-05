c49b7efffd84b8b9d5a97a2b3ab1f41e944789cc
childressdev <jcampbell@childressagency.com>
2017-12-05 10:36:46 -0500

    not absolute position

--- js/childressagency-scripts.js
+++ js/childressagency-scripts.js
@@ -1,3 +1,13 @@
+$.fn.accordionGrid = function(){
+  
+}
+
 jQuery(document).ready(function($){
+
+  $('.brand').hover(function(){
+    $(this).addClass('big-brand');
+  }, function(){
+    $(this).removeClass('big-brand');
+  });
 
 });
