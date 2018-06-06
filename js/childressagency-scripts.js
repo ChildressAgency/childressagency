faeb55ccf0937c0d3b897cba44d8b0d205aeaff4
childressdev <jcampbell@childressagency.com>
2018-06-06 16:39:16 -0400

    ready to animate in the first case study

--- js/childressagency-scripts.js
+++ js/childressagency-scripts.js
@@ -29,13 +29,8 @@ jQuery(document).ready(function($){
   var controller = new ScrollMagic.Controller();
 
   var slides = ['#hero', '#services', '#case-study1', '#case-study2', '#case-study3', '#contact'];
-
-  //var heroScene = new ScrollMagic.Scene({
-  //  triggerElement: '.hp-hero',
-  //  triggerHook: 0
-  //})
-  //.setClassToggle('.hp-hero', 'is-active')
-  //.addTo(controller);
+  
+  var $hpHero = $('.hp-hero');
 
   function loadComplete(){
     $('html, body').scrollTop(0);
@@ -43,41 +38,34 @@ jQuery(document).ready(function($){
 
     preloaderOutTl
       .set($('body'), { className: '-=is-loading' })
-      .set($('#hero'), { className: '+=is-loaded' })
-      .set($('.hp-hero'), {className: '+=is-active'})
+      .set($hpHero, { className: '+=is-loaded' })
       .to($('#pre-loader'), 0.7, {opacity: 0, ease:Power4.easeInOut})
       .set($('#pre-loader'), {className: '+=is-hidden'})
-      .staggerFromTo($('.slogan-list ul>li'), 1, {x: '+=500px'}, {x: 0, ease:Power1.easeOut}, .3);
+      .staggerFromTo($('.slogan-list ul>li'), .3, {xPercent: '120%'}, {xPercent: 0, ease:Power1.easeOut}, .3)
+      .set($hpHero, {className: '+=is-active'});
 
     return preloaderOutTl;
   }
 
-  var pinHeroTl = new TimelineMax();
+  var slideOutSloganTl = new TimelineMax();
 
-  pinHeroTl
-    .staggerTo($('.slogan-list ul>li'), 1, {x: '-=120%', ease:Power0.easeNone}, .3)
-    .to($('.hp-hero .overlay'), 1, {opacity: 1}, .5);
+  slideOutSloganTl
+    .staggerTo($('.slogan-list ul>li'), .5, {xPercent: '-=120%', ease:Power0.easeNone}, .3)
+    .to($('.hp-hero .overlay'), 1, {opacity: 1}, .5)
+    .fromTo($('.hp-hero-logo'), 1, {autoAlpha:0}, {autoAlpha:1}, 1)
+    .staggerFromTo($('.services-list ul>li'), 0.5, {y: '+=20', autoAlpha: 0}, {y: 0, autoAlpha:1, ease:Power0.easeIn}, 1.5)
+    .to($('.hp-hero .overlay'), 7, {width: '+=120%', delay: 12}, 2);
+    //.to($('.hp-hero-logo', $('.services-list'), 1, ))
+    //.set($('.hp-hero-logo'), {className: '+=fade-in'})
+    //.set($('.services-list'), {className: '+=fade-in-up'});
 
-  var pinHero = new ScrollMagic.Scene({
-    triggerElement: '.hp-hero',
-    triggerHook: '0',
-    duration: "100%"
+  var slideOutSlogan = new ScrollMagic.Scene({
+    triggerElement: '.page-wrapper',
+    triggerHook: 0,
+    duration: "700%"
   })
-  .setPin('.hp-hero')
-  .setTween(pinHeroTl)
+  .setPin('.page-wrapper')
+  .setTween(slideOutSloganTl)
   .addTo(controller);
 
-  var servicesScene = new ScrollMagic.Scene({
-    triggerElement: $('#services'),
-  })
-  .setClassToggle('#services', 'is-active')
-  .addTo(controller);
-
-/*  var sectionsParallaxUp = new ScrollMagic.Scene({
-    triggerElement: '#services',
-    triggerHook: 1,
-    duration: "100%"
-  })
-  .setTween(TweenMax.from('#services', 1, {y: '0%', ease:Power0.easeNone}))
-  .addTo(controller);*/
 });
