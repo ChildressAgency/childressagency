cb3ff1ec0b00ff88b70a57df4d2de5789df23d7b
childressdev <jcampbell@childressagency.com>
2018-06-29 16:48:00 -0400

    created

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="">
+
+  <meta http-equiv="cache-control" content="public">
+  <meta http-equiv="cache-control" content="private">
+
+  <title>The Childress Agency</title>
+
+  <script>document.documentElement.classList.remove('no-js');</script>
+
+  <?php wp_head(); ?>
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]--> 
+</head>
+
+<body class="is-loading">
+  <div id="pre-loader" class="full-screen pre-loader">
+    <div class="pre-loader-logo">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/childress-icon-lg.png" alt="Childress Agency Logo" />
+    </div>
+    <span class="wiper"></span>
+  </div>
+
+  <nav id="header-nav" class="navbar-fixed-top scroll-header">
+    <div class="container">
+      <div class="navbar-header white-nav">
+        <a href="#" class="navbar-brand"></a>
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+      <div id="navbar" class="navbar-collapse collapse white-nav">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="active"><a href="#">Who We Are</a></li>
+          <li><a href="#">Work</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
